b155e6260177233e08984bad8aeab2b1c9af0402
cmj <cmj@cmj.tw>
2026-09-15 16:20:26 +0800

    build: bump version to v2.1.2

--- cmd/baton/main.go
+++ cmd/baton/main.go
@@ -57,7 +57,7 @@ import (
 	"github.com/cmj0121/baton/internal/usage"
 )
 
-const version = "2.1.1"
+const version = "2.1.2"
 
 // daemonEnv marks the re-executed child that should run the server loop instead
 // of attaching a cockpit.
